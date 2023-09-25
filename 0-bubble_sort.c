b7a695a97ead3a7badba64e16983ecbd5685a9c5
n1Anouar <hakga76@gmail.com>
2023-09-25 09:00:33 +0000

    fix helo

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,4 @@
-nclude "sort.h"
+#include "sort.h"
 
 /**
  * bubble_sort - sorts an array of integers in ascending order,
--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,4 +1,4 @@
-nclude "sort.h"
+#include "sort.h"
 
 /**
  * insertion_sort_list - sorts a doubly linked list of integers,
